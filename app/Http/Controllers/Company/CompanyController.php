023a432215fdc843f5715052462a2755dbb40c27
jgonzales2025 <jgonzales@hsperu.pe>
2025-08-04 16:55:10 -0500

    feat: :sparkles: Controlador Company
    Se implementó el controlador para traer las compañias existentes.

---
+++ app/Http/Controllers/Company/CompanyController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Company;
+
+use App\Http\Controllers\Controller;
+use App\Models\Company;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class CompanyController extends Controller
+{
+    public function index(): JsonResponse
+    {
+        $companies = Company::all();
+
+        if ($companies->isEmpty()){
+            return new JsonResponse(['message' => 'No hay compañias registradas']);
+        }
+
+        return new JsonResponse($companies, 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,12 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\Customer\CustomerController;
+use App\Models\Company;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/customers', [CustomerController::class, 'store']);
 Route::post('/login', [AuthController::class, 'login']);
+Route::get('/companies', [Company::class, 'index']);
 
 Route::middleware('auth:sanctum')->group(function() {
     Route::get('/customers', [CustomerController::class, 'index']);
